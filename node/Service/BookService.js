739b9f0526261adb9ae3d675b2fe88b3716d6cdb
kevan <1129550978@qq.com>
2017-11-07 11:02:11 +0800

    书店项目第一次提交

---
+++ node/Service/BookService.js
@@ -0,0 +1,14 @@
+'use strict';
+let bookService=function(){
+	var monk = require('monk');
+	var db = monk('localhost:27017/BookStore');
+	
+	function Dao(){
+		this.getAllBooks=function(){
+			return db.get('books');
+		}
+	}
+	
+}
+
+module.exports = bookService;
